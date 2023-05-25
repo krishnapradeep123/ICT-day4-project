1a16232b643da581de84d4f3fefd1ba07bb28f8e
krishnapradeep123 <Krishnakinnuz@gmail.com>
2023-05-25 12:12:40 +0530

    second commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="login">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+   
+    </>
+  );
+}
+
+export default ColorSchemesExample;
